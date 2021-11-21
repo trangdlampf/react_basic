35416299ef773a86b74476b8c559e5b41d989ace
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2021-11-21 16:27:03 -0500

    axios, useEffect, onClick
    axios, useEffect, onClick

---
+++ cV_1axios_useEffect/DataFetching2_onClick.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DataFetching() {
+
+    const [post, setPost ] = useState();
+    const [id, setId] = useState(1);
+
+    useEffect( () => {
+        axios
+            .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => {
+                console.log(res);
+                setPost(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [id]);
+    return (
+        <div>
+            <input type="text" value={id} onChange={e => setId(e.target.value)} />
+            <div>{post.title}</div>
+            {/* <ul>{ 
+                posts.map( posts => <li key={posts.id}>
+                    {posts.title}
+                </li>)        
+            }</ul> */}         
+        </div>
+    )
+}
+
+export default DataFetching;
