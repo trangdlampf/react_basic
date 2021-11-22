84eee729d7d9de363169513efc17fb0c27043ea0
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2021-11-22 10:22:43 -0500

    getData w/Axios, useEffect, button
    getData w/Axios, useEffect, button

--- cV_1axios_useEffect/DataFetching3_button.js
+++ cV_1axios_useEffect/DataFetching3_button.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-function DataFetching() {
+const GetData3 = () => {
 
-    const [post, setPost ] = useState({});
+    const [post, setPost] = useState([]);
     const [id, setId] = useState(1);
-    const [idFromButtonClick, setIdFromButtonClick ] = useState(1); 
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1);
 
-    const handleClick= () => {
+    const handleClick = () =>{
         setIdFromButtonClick(id);    
     }
 
     useEffect( () => {
+
         axios
-            .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`) // originally id
             .then(res => {
                 console.log(res);
                 setPost(res.data);
@@ -21,15 +22,24 @@ function DataFetching() {
             .catch(err => {
                 console.log(err);
             });
-    }, [idFromButtonClick]);
-    return (
-        <div>
-            <input type="text" value={id} onChange={e => setId(e.target.value)} />
-            <button type="button" onClick={handleClick}>Fetch Post</button>
-            
-            <div>{post.title}</div>  
-        </div>
+    }, [idFromButtonClick] // originally was 'id' to 'idFromButtonClick'
     )
-}
 
-export default DataFetching;
+    return (  
+        <div>
+            <p>GetData3</p>
+            <input type="text" value={id} onChange={ e => setId( e.target.value )} />
+            <button type="button" value={id} onClick={handleClick}>Click to Get Data</button>
+            <div key={post.id} >{post.title}</div>
+            { /* <div>{
+                posts.map(
+                    post => <li key={post.id}>
+                        {post.title}
+                    </li>    
+                )
+            }</div> */ }
+        </div>
+    );
+}
+ 
+export default GetData3;
