711d31e8d0553c76c4bbe911af0a513287b74e1b
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2021-11-23 13:10:17 -0500

    App.js axios useState useEffect
    App.js axios useState useEffect

---
+++ mygetdata/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+// import GetDataF from "./GetData";
+import GetDataF from './GetDataF';
+
+function App() {
+  return (
+    <div className="App">
+      <GetDataF />
+    </div>
+  );
+}
+export default App;
